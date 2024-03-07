3801450297882aea4d9dfa4f0757deaec6e2ee89
Pietro Bucker <pietro_177@hotmail.com>
2024-03-07 13:32:56 -0300

    inciando app

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import {apiRequire} from './context/Fetch';
+import Home from './component/Home';
+import {ClienteType} from '../../back/src/model/ClienteType';
 
 function App() {
+  const [clientes, setClientes] = useState<ClienteType[]>([]);
+  const [nome, setNome] = useState('');
+  
+  useEffect(() => {
+    apiRequire().then((data) => {
+      setClientes(data);
+    });
+  }, []);
+
+
   return (
     <div>
-      <p>Heloword</p>
+      <Home clientes={clientes} />
+
     </div>
   );
 }
+
 
 export default App;
