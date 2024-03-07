03442331f97308c8c992c3ecaf00a72b3acbb9a3
Pietro Bucker <pietro_177@hotmail.com>
2024-03-07 20:25:12 -0300

    add component no App

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import {apiRequire} from './context/Fetch';
 import Home from './component/Home';
 import {ClienteType} from '../../back/src/model/ClienteType';
+import Cadastro from './component/Cadastro';
 
 function App() {
   const [clientes, setClientes] = useState<ClienteType[]>([]);
@@ -16,6 +17,7 @@ function App() {
 
   return (
     <div>
+      <Cadastro />
       <Home clientes={clientes} />
     </div>
   );
