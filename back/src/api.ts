340ec3dd44983146c4b3c7d9d18deaf85eaba7fd
Pietro Bucker <pietro_177@hotmail.com>
2024-03-07 13:32:24 -0300

    configuraçao inicial do cors para

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,12 +1,17 @@
 import express, {Response, Request} from 'express';
 import connection from './db/connection';
 import ClienteModel from './model/ClienteModel';
+import cors from 'cors';
 
 const model = new ClienteModel(connection);
 
+const corsConfig = {
+    origin: 'http://localhost:3000',
+}
 const api = express();
 
 api.use(express.json());
+api.use(cors())
 
 api.get('/', async (req: Request, res: Response): Promise<Response> => {
     try{
