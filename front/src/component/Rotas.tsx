41cab3a09fd13ebcc2052f2c85f4b22f6d618a15
Pietro Bucker <pietro_177@hotmail.com>
2024-03-08 18:06:57 -0300

    add classe e passado style para dentro do css

--- front/src/component/Rotas.tsx
+++ front/src/component/Rotas.tsx
@@ -1,34 +1,11 @@
+import './styles/StyleRotas.css'
 import React from 'react'
 import Modal from 'react-modal';
 import { apiRouteRequire } from '../context/Fetch';
 import { ClienteType } from '../../../back/src/model/ClienteType';
 
 Modal.setAppElement('#root');
-const modelStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    
-  },
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
- 
-  };
 
-const modelContentStyles: React.CSSProperties = {
-    
-      display: 'flex',
-      justifyContent: 'space-between',
-      border: '1px solid black',
-      padding: '10px',
-      margin: '10px',
-      
-  };
 export default function Rotas() {
     const [modalIsOpen, setIsOpen] = React.useState(false);
     const [rotas, setRotas] = React.useState<ClienteType[]>([]);
@@ -47,28 +24,42 @@ export default function Rotas() {
         })
       }
   return (
-    <div>
+    <div className='rota_container'>
+
         <button
-            onClick={openModal}
-        >Buscar Rotas</button>
+            className='botao_rotas'
+            onClick={openModal}>🌐</button>
         <Modal
+            
             isOpen={modalIsOpen}
             onRequestClose={closeModal}
-            style={modelStyles}
+            className='customModal'
+            overlayClassName='customOverlay'
+            // style={modelStyles}
             contentLabel="Example Modal"
         >
-
+          <table className='rota_content'>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Telefone</th>
+                <th>localizacao</th>
+              </tr>
+            </thead>
           {rotas.map((rota, index) => (
-              <div style={modelContentStyles} key={index}>
-                  <p>{rota.nome}</p>
-                  <p>{rota.email}</p>
-                  <p>{rota.telefone}</p>
-              </div>
+            <tr key={index}>
+                  <td>{rota.nome}</td>
+                  <td>{rota.email}</td>
+                  <td>{rota.telefone}</td>
+                  <td>({rota.localizacao.x},{rota.localizacao.y})</td>
+
+              </tr>
           
           ))}
-            <button
-                onClick={closeModal
-            }>Fechar</button>
+          </table>
+          
+           
         </Modal>
 
     </div>
