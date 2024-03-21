58bd7054fad3e2a24c3334dfa4795a8ad8b83a1e
Pietro Bucker <pietro_177@hotmail.com>
2024-03-21 19:11:08 -0300

    commt bomba, criado pagina cliente

---
+++ front-native/src/app/Clientes.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { Text, TouchableOpacity, View, TouchableOpacityProps } from "react-native";
+
+import { Header } from "@/components/Header";
+import { Loading } from "@/components/Loading";
+
+import { ClienteType } from "../../../back/src/model/ClienteType";
+import { apiRequire, apiRouteRequire } from "@/utils/fetch";
+import { CardClient } from "@/components/CardClient";
+import ModalClientes from "@/components/ModalClientes";
+import { Button } from "@/components/Button";
+
+
+export default function Clientes(...rest: TouchableOpacityProps[]) {
+    const [clientes, setClientes] = useState<ClienteType[]>([])
+    const [modalVisible, setModalVisible] = useState<boolean>(false)
+    const [orderByLocalizacao, setOrderByLocalizacao] = useState<ClienteType[]>([])
+
+    useEffect(() => {
+        apiRequire().then(data => setClientes(data))
+
+    }, [])
+
+    if (clientes.length === 0) {
+        return (<Loading />)
+    }
+
+    function requestOrderByLocal() {
+        apiRouteRequire().then(data => setOrderByLocalizacao(data))
+    }
+    return (
+        <View className="flex-1">
+            <View className="">
+                <Header title="Clientes" />
+            </View>
+            <View className="flex-1">
+                <Button title='' />
+                <CardClient clientes={clientes} />
+            </View>
+            <View className="">
+                <ModalClientes isVisible={modalVisible} setVisible={setModalVisible} orderClientes={orderByLocalizacao} />
+                <TouchableOpacity
+                    className=""
+                    onPress={() => {
+                        requestOrderByLocal()
+                        setModalVisible(true)}}
+                >
+                    <Text className="bg-blue-500 text-white text-center p-5 mt-10">Localização</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
